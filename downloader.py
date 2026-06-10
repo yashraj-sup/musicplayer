499cdb10da3a7337917e069826bfd1a4485f6b63
yashraj-sup <yttfc29062024@gmail.com>
2026-06-10 07:05:28 +0530

    add threading, Player class,working playback controls

--- downloader.py
+++ downloader.py
@@ -1,7 +1,7 @@
 import yt_dlp
 def download(url,output_folder):
     try:
-        ydl_opts={"format":"bestaudio/best","outtmpl":output_folder +"/%(title)s.%(ext)s","postprocessors":[{"key":"ffmpegExtractAudio", "preferredcodec":"mp3"}]}
+        ydl_opts={"format":"bestaudio/best","outtmpl":output_folder +"/%(title)s.%(ext)s","postprocessors":[{"key":"FFmpegExtractAudio", "preferredcodec":"mp3"}]}
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
     except yt_dlp.utils.DownloadError as e:
@@ -10,6 +10,6 @@ def download(url,output_folder):
             print(f"Invalid url:{e}")
     except Exception as e:
             print(f"Unexpected error:{e}")
-download("https://youtu.be/dXZYOqqg6Kk?si=kT-LfuDstd3S2qgj","/home/yash/Desktop/musicplayer")
+download("https://youtu.be/IXMEt7ro0x0?si=JoDFkN_ghpSIKoXR","/home/yash/Desktop/musicplayer")
 
 
--- player.py
+++ player.py
@@ -6,10 +6,8 @@ def scan_folder(folder_path):
     files=list(folder.glob("*.mp3"))
     return files 
 def play(file_path):
-    pygame.mixer.init()
     pygame.mixer.music.load(file_path)
     pygame.mixer.music.play()
-files=scan_folder("/home/yash/Desktop/musicplayer")
 class Player:
     def __init__(self,files):
         self.files=files
@@ -19,6 +17,7 @@ class Player:
         file_path=self.files[self.index]
         play(file_path)
         self.state="playing"
+        print(f"Now playing: {self.files[self.index].name}")
     def next(self):
         self.index+=1
         if self.index==len(self.files):
@@ -30,5 +29,32 @@ class Player:
     def resume(self):
         self.state="playing"
         pygame.mixer.music.unpause()
-play(files[0])
-time.sleep(20)
+    def monitor(self):
+        while True:
+            if pygame.mixer.get_init() and pygame.mixer.music.get_busy()==False and self.state=="playing":
+                self.next()
+            time.sleep(1)
+    def input_loop(self):
+        while True:
+            command=input("Enter a command:")
+            if command=="pause":
+                self.pause()
+                print("paused")
+            elif command=="resume":
+                self.resume()
+                print("resumed")
+            elif command=="next":
+                self.next()
+                print(f"Playing: {self.files[self.index].name}")
+            elif command=="quit":
+                break
+pygame.mixer.pre_init(44100,-16,2,2048)
+pygame.mixer.init()
+files=scan_folder("/home/yash/Desktop/musicplayer")
+player=Player(files)
+import threading
+t=threading.Thread(target=player.monitor)
+t.daemon=True
+t.start()
+player.play_current()
+player.input_loop()
